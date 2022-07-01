fc951c95ef0bf3a78417be62f685e1982642516c
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-07-01 00:53:12 -0400

    configuracion exitosa del correo, nodemailer funcionando con el formulario de contacto en React, falta agregar todo el tema del portafolio y configurar las imagenes y agregar proyectos al portafolio y quedaria listo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const { Router} = require('express');
-const nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
 const router = Router();
 
+//Variables de entorno
+var { config } = require('dotenv');
+config();
 
-router.post('/send-email', (req,res) => {
+console.log(process.env.user)
+
+
+router.post('/send-email', async (req,res) => {
     const {nombre_completo, correo, telefono, asunto, mensaje} = req.body;
 
     contentHTML = `
@@ -16,15 +22,30 @@ router.post('/send-email', (req,res) => {
         </ul>
         <p> ${mensaje}</p>
     `
-    nodemailer.createTransport({
-        host: '',
-        port:,
-        secure:,
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
         auth:{
-            user: '',
-            
+            user: process.env.User,
+            pass: process.env.Pass,
         }
-    })
+    });
+
+    const message = await transporter.sendMail({
+        from: " 'Nicolas Server' <process.env.User>",
+        to: 'nicolas.bouffanais.1999@gmail.com',
+        subject: "Contacto de mi pagina Web",
+        html: contentHTML
+    });
+
+    transporter.sendMail(message, function (err, res) {
+        if (err) {                                            // if error
+            console.log(err);
+        } else {
+            console.log(res);                                // if success
+        }
+    });
+
+    console.log("Mensaje enviado", message.messageId);
 
 
     res.send('recibido');
